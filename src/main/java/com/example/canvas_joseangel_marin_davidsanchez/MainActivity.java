cd14aa683f6d7ab41b83ffdcf182e07217442362
David Sánchez Morales <78800247+DavidSM24@users.noreply.github.com>
2022-01-10 09:52:06 +0100

    Add files via upload

---
+++ src/main/java/com/example/canvas_joseangel_marin_davidsanchez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.canvas_joseangel_marin_davidsanchez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
